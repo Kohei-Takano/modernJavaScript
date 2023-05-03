c887590b0e1bd577f72380db71aa2fe98f3d1911
Kohei-Takano <132127471+Kohei-Takano@users.noreply.github.com>
2023-05-04 00:00:21 +0900

    add //

--- src/index.js
+++ src/index.js
@@ -31,16 +31,16 @@
 // const val3="const変数を再宣言";
 
 //constで定義したオブジェクトはプロパティの変更が可能
-const val4 = {
-  name: "じゃけぇ",
-  age: 28
-};
-val4.name = "jak";
-val4.address = "Hiroshima";
-console.log(val4);
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
 
-//constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// //constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
